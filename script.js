958973800a37f4f3a4a606e9abc2a97884b19b37
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-26 13:57:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const body=document.querySelector("body");
+const btn=document.getElementById("openModal");
+const div=document.querySelector(".modal");
+btn.addEventListener("click",(e)=>{
+	body.style.backgroundColor="#707170";
+	div.style.top="20%";
+	div.classList.add("afterClick");
+})
